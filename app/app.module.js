587070bce688c09421dc32ca587d35243719fec4
karvel <jadeilyn@yahoo.com>
2016-03-01 10:32:36 -0800

    Restored CSS.

--- app/app.module.js
+++ app/app.module.js
@@ -1,13 +1,18 @@
 angular.module('app', [
-    'templates',
-    'ui.router',
-    // 'welcome',
-    'todoApp'
-  ])
-  .config(function ($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/todo');
-    //$urlRouterProvider.otherwise('/welcome');
-  })
-  .run(function () {
+        'templates',
+        'ui.router',
+        // 'welcome',
+        'todoApp'
+    ])
+    .config(function($urlRouterProvider, $stateProvider) {
+        $stateProvider
+            .state('todoApp', {
+                url: '',
+                abstract: true
+            });
+        $urlRouterProvider.otherwise('/todo');
+        //$urlRouterProvider.otherwise('/welcome');
+    })
+    .run(function() {
 
-  });
+    });
